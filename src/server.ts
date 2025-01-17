7395c77d232be8a280409c47692430e2a762af7a
Allysson Ryan <allyssonr002@gmail.com>
2025-01-16 22:04:00 -0300

    update

--- src/server.ts
+++ src/server.ts
@@ -10,12 +10,10 @@ setupMongo().then(() => {
 
   // Configuração do CORS
   app.use(
-    cors({
-      origin: process.env.FRONT_URL, // URL do seu frontend
-      methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
-      allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
-    }),
-  );
+        cors({
+          origin: process.env.FRONT_URL,
+        }),
+      );
 
   app.use(json());
   app.use(routes);
@@ -24,25 +22,3 @@ setupMongo().then(() => {
   const port = process.env.PORT || 3334;
   app.listen(port, () => console.log(`🚀 App is running on port ${port}!`));
 });
-
-// import express, { json } from 'express';
-// import { routes } from './routes';
-// import 'dotenv/config';
-// import cors from 'cors';
-// import { setupMongo } from './database';
-// import { errorHandler } from './middleware/error.handler.middleware';
-
-// setupMongo().then(() => {
-//   const app = express();
-
-//   app.use(
-//     cors({
-//       origin: process.env.FRONT_URL,
-//     }),
-//   );
-//   app.use(json());
-//   app.use(routes);
-//   app.use(errorHandler);
-
-//   app.listen(3333, () => console.log('🚀 App is running at 3333!'));
-// });
